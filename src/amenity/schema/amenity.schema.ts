8e129bb32eee5f7c9f26478c312ba65fe9f5af34
EyadAymanM <111561121+EyadAymanM@users.noreply.github.com>
2024-10-09 23:11:32 +0300

    added from prop to amenities

--- src/amenity/schema/amenity.schema.ts
+++ src/amenity/schema/amenity.schema.ts
@@ -3,8 +3,11 @@ import { Document } from 'mongoose';
 
 @Schema()
 export class Amenity extends Document {
-  @Prop({ required: true , unique:true })
+  @Prop({ required: true, unique: true })
   name: string;
+
+  @Prop({ required: true, unique: true })
+  from: string;
 
   @Prop({ required: true })
   description: string;
