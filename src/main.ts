f429ccadaae8b2b8ef5b101fe3058f7b98c27526
EyadAymanM <111561121+EyadAymanM@users.noreply.github.com>
2024-11-06 17:00:47 +0200

    adding cors to the vercel app

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({});
+  app.enableCors({
+    origin: [
+      'http://localhost:3001',
+      'https://airbnb-clone-nextjs-henna.vercel.app',
+    ],
+    methods: ['GET', 'POST', 'HEAD', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+});
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
