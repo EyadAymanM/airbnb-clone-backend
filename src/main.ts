e1797aae1f55b30527eb1ca54f5c50c6cce8acb5
EyadAymanM <111561121+EyadAymanM@users.noreply.github.com>
2024-11-06 17:31:59 +0200

    reverting cors changes

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:3001',
-      'https://airbnb-clone-nextjs-henna.vercel.app',
-    ],
-    methods: ['GET', 'POST', 'HEAD', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    credentials: true,
-});
+  app.enableCors({});
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
