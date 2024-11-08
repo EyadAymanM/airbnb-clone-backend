7a439b2fa20634ecc8b67fe42c68deae996a1e41
EyadAymanM <111561121+EyadAymanM@users.noreply.github.com>
2024-11-08 18:13:04 +0200

    trying to fix cors for dashboard

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://airbnb-clone-dashboard2.vercel.app',
+      'https://airbnb-clone-nextjs-henna.vercel.app/',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
